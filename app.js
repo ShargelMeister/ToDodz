39fab70dc370c98d2ab5536b5b85a484b4f0aced
ShargelMeister <shargelmeister@gmail.com>
2021-08-15 23:52:18 +0300

    fix
    add localStorage
    add moveTodo

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const addBtn = document.querySelector('.addBtn');
 const todoItems = document.querySelector('.todo-items');
 const closes = document.querySelector('.closeBtn');
 const template = document.querySelector('.template');
-const readyTodoItems = document.querySelector('ready-todo-items');
-const readyButton = document.querySelector('ready-button')
-
-
+const readyTodoItems = document.querySelector('.ready-todo-items');
 
 addBtn.addEventListener('click', () => {
     createTodoItem();
+    todoLocal();
 })
 function createTodoItem() { 
     const newTodo = template.cloneNode(true);
@@ -17,6 +15,10 @@ function createTodoItem() {
     text.innerHTML = input.value;
     newTodo.classList.remove('template');
     todoItems.appendChild(newTodo);
+};
+function todoLocal(todoLocals) {
+    todoLocals = todoItems.innerHTML;
+    localStorage.setItem('todo',todoLocals);
 };
 function deleteItem (todoItem) {
     if (!todoItem) {
@@ -31,6 +33,18 @@ function onClickActive(todoItem) {
 function onClickChecked(todoItem) {
     todoItem.classList.toggle('checked');
     todoItem.classList.toggle('active');
+};
+function onReadyClickTodo(todoItem) {
+    const readyTodoItem = todoItem.cloneNode(true);
+    readyTodoItem.classList.add('todo-item-ready');
+    readyTodoItems.appendChild(readyTodoItem);
+    deleteItem(todoItem);
+};
+function onReadyBackClickTodo(todoItem) {
+    const backTodoItem = todoItem.cloneNode(true);
+    backTodoItem.classList.remove('todo-item-ready');
+    todoItems.appendChild(backTodoItem);
+    deleteItem(todoItem);
 };
 function showInput(todoItem) {
     const editForm = todoItem.getElementsByClassName('edit-form')[0];
@@ -48,32 +62,46 @@ function editInput(todoItem) {
 document.addEventListener('click',function(e){
     if (!e.target) {
         return;
-    };
+    }
     if(e.target.classList.contains('closeBtn')){
         deleteItem(e.target.closest('.todo-item'));
-    };
+        todoLocal();
+        return;
+    }
+    if(e.target.classList.contains('back-button')){
+        onReadyBackClickTodo(e.target.closest('.todo-item'));
+        todoLocal();
+        return;
+    }
+    if(e.target.classList.contains('ready-button')){
+        onReadyClickTodo(e.target.closest('.todo-item'));
+        todoLocal();
+        return;
+    }
     if (e.target.classList.contains('edit-button')){
         editInput(e.target.closest('.todo-item'));
+        todoLocal();
         return;
-    };
+    }
     if (e.target.classList.contains('edit')){
         showInput(e.target.closest('.todo-item'));
+        todoLocal();
         return;
-    };
+    }
     if(e.target.closest('active')){
         onClickActive(e.target.closest('active'));
         return;
-    };
+    }
     if(e.target.classList.contains('active')){
         onClickActive(e.target);
         return;
-    };
+    }
     if(e.target.closest('checked')){
         onClickChecked(e.target.closest('checked'));
         return;
-    };
+    }
     if(e.target.classList.contains('checked')){
         onClickChecked(e.target);
         return;
-    };
+    }
  });
